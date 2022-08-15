fe1347e89ff8c7e1544a9de47271a9787ea3d198
2Peti <pney0807@gmail.com>
2022-08-15 22:35:06 +0200

    Use readline. Add readme and Colab notebook.

---
+++ README.md
@@ -0,0 +1,28 @@
+# MCSeeker
+
+MCSeeker finds minecraft servers in list of IPs and Ports.
+
+MCSeeker with Masscan on Google Colab:
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/2Peti/mcseeker/blob/main/colab.ipynb)
+
+## Usage
+
+```bash
+node index.js --in inputfile.txt --out output.csv
+```
+Input file should be in format ```IP:Port```.
+
+Example input file: ```
+1.1.1.1:25565
+2.2.2.2:25565
+```
+
+Output is in format ```IP:Port,Version,Online/Max Players,MOTD```. (see exampleOUT.csv)
+
+Example output file: ```
+1.1.1.1:25565,1.19,12/20,A Minecraft Server
+2.2.2.2:25565,1.19,25/30,A Minecraft Server
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var fs = require('fs');
+const fs = require('fs');
 var status = require('minecraft-status').MinecraftServerListPing;
+const events = require('events');
+const readline = require('readline');
 process.params = (require('commandos')).parse(process.argv);
 
 var INPUT_FILE = (process.params['in']);
@@ -7,29 +9,29 @@ var SCAN_OPTS_OUTPUT_CSV = (process.params['out']);
 var TIMEOUT = (process.params['timeout'] || 15000);
 
 var outStream = fs.createWriteStream(SCAN_OPTS_OUTPUT_CSV, { flags: 'a' });
-var i = 0;
+var servers = 0;
+var total = 0;
 
-function a(file_line){
-    var data = file_line.split(":");
-    status.ping(757, data[0], data[1], TIMEOUT).then(function(pingRes){
-      i++;
-      //console.log(`${file_line}\t${pingRes.version.name}\t${pingRes.players.online} of ${pingRes.players.max} players\t${pingRes.description.text.replace(/\n/g, ' ').replace(/\u00A7[0-9A-FK-OR]/ig,'')}`);
-      outStream.write(`${file_line},${pingRes.version.name.replace(/\,/g, '+')},${pingRes.players.online}/${pingRes.players.max},${pingRes.description.text.replace(/\n/g, ' ').replace(/\,/g, ';')}\n`);
-      if (pingRes.description.text.toLowerCase().includes('LiveOverflow'.toLowerCase())) {
-        console.log(`Found LiveOverflow on ${file_line}`);
-      }
-    }).catch(function(error){
-      //console.log(error);
-    });
+main();
+printProgress();
+async function main() {
+  console.log("Scanning IPs from " + INPUT_FILE);
+  var inFile = readline.createInterface({input: fs.createReadStream(INPUT_FILE)});
+  inFile.on('line', function (line) {
+    var data = line.split(":");
+    status.ping(757, data[0], data[1], TIMEOUT).then((pingRes) => {
+      servers++
+      total++
+      //console.log(`${line}\t${pingRes.version.name}\t${pingRes.players.online} of ${pingRes.players.max} players\t${pingRes.description.text.replace(/\n/g, ' ').replace(/\u00A7[0-9A-FK-OR]/ig,'')}`);
+      outStream.write(`${line},${pingRes.version.name.replace(/\,/g, '+')},${pingRes.players.online}/${pingRes.players.max},${pingRes.description.text.replace(/\n/g, ' ').replace(/\,/g, ';')}\n`);
+    }).catch((error) => {total++});
+  });
 }
-
-console.log("Scanning IPs from " + INPUT_FILE);
-console.time("finish");
-console.time("load");
-var lines = fs.readFileSync(INPUT_FILE, 'utf-8').split('\n');
-for (var x = 0; x < lines.length; x++) {
-  a(lines[x]);
+async function printProgress(){
+  await new Promise(resolve => setTimeout(resolve, 1000))
+  for (var x = 0; x < total+1; x++) {
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Found ${servers}/${total} servers`);
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
 }
-console.timeEnd("load");
-function exitHandler() {console.timeEnd("finish");console.log(`Found ${i} servers.`);}
-process.on('exit', exitHandler.bind());
