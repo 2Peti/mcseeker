cb30ae3ae9b89ddaf86e37be6579f2b7e2622174
2Peti <pney0807@gmail.com>
2022-08-16 14:59:55 +0200

    add test and support for mc 1.8

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
-var status = require('minecraft-status').MinecraftServerListPing;
-const events = require('events');
+const mcstatus = require('minecraft-status').MinecraftServerListPing;
 const readline = require('readline');
-process.params = (require('commandos')).parse(process.argv);
+process.params = require('commandos').parse(process.argv);
 
 var INPUT_FILE = (process.params['in']);
-var SCAN_OPTS_OUTPUT_CSV = (process.params['out']);
+var OUTPUT_FILE = (process.params['out']);
 var TIMEOUT = (process.params['timeout'] || 15000);
+var QUIET = (process.params['quiet'] || false);
 
-var outStream = fs.createWriteStream(SCAN_OPTS_OUTPUT_CSV, { flags: 'a' });
 var servers = 0;
 var total = 0;
 
 main();
-printProgress();
+if (!QUIET){printProgress();}
+
 async function main() {
+  var outStream = fs.createWriteStream(OUTPUT_FILE, { flags: 'a' });
   console.log("Scanning IPs from " + INPUT_FILE);
   var inFile = readline.createInterface({input: fs.createReadStream(INPUT_FILE)});
   inFile.on('line', function (line) {
     var data = line.split(":");
-    status.ping(757, data[0], data[1], TIMEOUT).then((pingRes) => {
-      servers++
-      total++
-      //console.log(`${line}\t${pingRes.version.name}\t${pingRes.players.online} of ${pingRes.players.max} players\t${pingRes.description.text.replace(/\n/g, ' ').replace(/\u00A7[0-9A-FK-OR]/ig,'')}`);
-      outStream.write(`${line},${pingRes.version.name.replace(/\,/g, '+')},${pingRes.players.online}/${pingRes.players.max},${pingRes.description.text.replace(/\n/g, ' ').replace(/\,/g, ';')}\n`);
+    mcstatus.ping(757, data[0], data[1], TIMEOUT).then((pingRes) => {
+      servers++;
+      total++;
+      desc = (pingRes.description.text) ? (pingRes.description.text) : (pingRes.description)
+      //console.log(`${line}\t${pingRes.version.name}\t${pingRes.players.online} of ${pingRes.players.max} players\t${desc.replace(/\n/g, ' ').replace(/\u00A7[0-9A-FK-OR]/ig,'')}`);
+      outStream.write(`${line},${pingRes.version.name.replace(/\,/g, '+')},${pingRes.players.online}/${pingRes.players.max},${desc.replace(/\n/g, ' ').replace(/\,/g, ';')}\n`);
     }).catch((error) => {total++});
   });
 }
 async function printProgress(){
   await new Promise(resolve => setTimeout(resolve, 1000))
-  for (var x = 0; x < total+1; x++) {
+  for (var x = 0; x < total; x++) {
     process.stdout.cursorTo(0);
     process.stdout.write(`Found ${servers}/${total} servers`);
     await new Promise(resolve => setTimeout(resolve, 1000))
---
+++ index.test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const events = require('events');
+const { fork,exec  } = require("child_process");
+
+var myData = "mc.hypixel.net:25565";
+
+test("Hypixel", async () => {
+  let tmpDir;
+  var data;
+  try {
+    tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "mcseeker"));
+    inFile = tmpDir + "/input.txt";
+    outFile = tmpDir + "/output.csv";
+    fs.writeFileSync(inFile, myData);
+    const child = fork("index.js", ["--in", inFile, "--out", outFile, "--quiet"]);
+    await events.once(child, 'close');
+
+    try {
+      data = fs.readFileSync(outFile, 'utf8').toString();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  catch (err) {console.error(err);}
+  finally { 
+    try {
+      if (tmpDir) {fs.rmSync(tmpDir, { recursive: true });}
+    }catch (err) {
+      console.error(`An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${err}`);
+    }
+  }
+  expect(data.substring(0,myData.length)).toEqual(myData);
+});
