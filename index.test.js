94f2a3b5facd2ae829191432afa16dadbcdf3078
2Peti <pney0807@gmail.com>
2022-08-16 18:15:20 +0200

    Update test

--- index.test.js
+++ index.test.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const events = require('events');
-const { fork,exec  } = require("child_process");
+const { fork } = require("child_process");
 
-var myData = "mc.hypixel.net:25565";
+var testServer = "mc.hypixel.net:25565";
 
-test("Hypixel", async () => {
+test(testServer, async () => {
   let tmpDir;
   var data;
   try {
     tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "mcseeker"));
     inFile = tmpDir + "/input.txt";
     outFile = tmpDir + "/output.csv";
-    fs.writeFileSync(inFile, myData);
+    fs.writeFileSync(inFile, testServer);
     const child = fork("index.js", ["--in", inFile, "--out", outFile, "--quiet"]);
     await events.once(child, 'close');
 
@@ -31,5 +31,6 @@ test("Hypixel", async () => {
       console.error(`An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${err}`);
     }
   }
-  expect(data.substring(0,myData.length)).toEqual(myData);
+  expect(data.substring(0,testServer.length)).toBe(testServer);
+  expect(data.length).toBeGreaterThan(testServer.length);
 });
